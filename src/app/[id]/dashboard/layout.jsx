942c0eae352634f5a72aa8576e26de2dd8435b2b
Shubham Manohar <shubhammanohar20@gmail.com>
2026-04-04 23:34:02 +0530

    sidebar

--- src/app/[id]/dashboard/layout.jsx
+++ src/app/[id]/dashboard/layout.jsx
@@ -1,6 +1,14 @@
+import Sidebar from "@/components/dashboard/Sidebar";
 import Protectedlayout from "@/layouts/ProtectedLayout";
 import React from "react";
 
 export default function RootLayout({ children }) {
-	return <Protectedlayout>{children}</Protectedlayout>;
+	return (
+		<Protectedlayout>
+			<div className="min-h-screen bg-slate-50 flex">
+				<Sidebar />
+				<main className="flex-1 p-6 lg:p-10">{children}</main>
+			</div>
+		</Protectedlayout>
+	);
 }
