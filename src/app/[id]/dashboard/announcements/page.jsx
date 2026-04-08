153902421b699907cc0be170773f8d298965f5a5
Shubham Manohar <shubhammanohar20@gmail.com>
2026-04-09 00:52:13 +0530

    announcement UI

--- src/app/[id]/dashboard/announcements/page.jsx
+++ src/app/[id]/dashboard/announcements/page.jsx
@@ -1,14 +1,13 @@
 import AnnouncementsPage from "@/views/dashboard/Announcements";
-import ProfilePage from "@/views/dashboard/Profile";
 import React, { memo } from "react";
 
 export const metadata = {
-	title: "Profile page",
-	description: "User profile information",
+	title: "Announcement Information",
+	description: "School and class announcements",
 };
 
-const ProfileUi = () => {
+const AnnouncementUi = () => {
 	return <AnnouncementsPage />;
 };
 
-export default memo(ProfileUi);
+export default memo(AnnouncementUi);
